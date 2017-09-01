2592d1b0db2aa73530d16282d5a74fd12b9c805f
Jean-Frederic <jeanfrederic.wiki@gmail.com>
2017-09-01 10:08:18 +0100

    Add more logging to Shell update script
    This helps when triggering manual updates to understand
    what is going on.

--- bin/update_database.sh
+++ bin/update_database.sh
@@ -2,5 +2,14 @@
 #
 # Script to update the JSON database
 
+echo_time() {
+    echo "$(date +%F_%T) $*"
+}
+
 cd ~/wikiloves || exit
+
+echo_time "Starting database update."
+
 python database.py update
+
+echo_time "Done with the update!"
