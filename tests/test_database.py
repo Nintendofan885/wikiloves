05bcf3caa1e6613f5ee63acaed1780b7b0dfc922
Jean-Frederic <jeanfrederic.wiki@gmail.com>
2017-11-29 18:54:33 +0000

    Fix tests for reData
    `reData` takes two arguments, not one... and it always been the case.

--- tests/test_database.py
+++ tests/test_database.py
@@ -10,7 +10,7 @@ class TestReData(unittest.TestCase):
 
     def test_reData_event_line(self):
         input_data = 'wl["monuments"][2010] = {'
-        result = database.reData(input_data)
+        result = database.reData(input_data, 2014)
         expected = {
             u'country': None,
             u'year': '2010',
@@ -24,7 +24,7 @@ class TestReData(unittest.TestCase):
         input_data = '''
     ["az"] = {["start"] = 20170430200000, ["end"] = 20170531195959},
     '''
-        result = database.reData(input_data)
+        result = database.reData(input_data, 2017)
         expected = {
             u'country': 'az',
             u'year': None,
