Nintendofan885/wikiloves

commit d2e89930a2f9470b648315ca3c3893209ab99c2e

Ensure country names are normalized from the URL
Several URL routes match a country name, eg
</monuments/2018/United_Arab_Emirates/users>.

This country name is used to look up the right
data from the database (as dictionary keys).

When a country is composed of several words,
the URL may have underscores, resulting in the
country not being found.

This ensures that the underscores are replaced
with spaces first thing after reading from the URL.

app.py
@@ -19,7 +19,8 @@ from functions import (
     get_events_data,
     get_instance_name,
     get_instance_users_data,
-    get_menu
+    get_menu,
+    normalize_country_name
 )
 
 app = Flask(__name__)
@@ -106,6 +107,7 @@ def users(name, year, country):
     if not db:
         return index()
     year = '20' + year
+    name = normalize_country_name(name)
     edition_slug = name + year
     if edition_slug in db and country in db[edition_slug]:
         instance_name = get_instance_name(name, year, country)
@@ -124,6 +126,7 @@ def instance(name, year, country):
         return index()
     year = '20' + year
     edition_slug = name + year
+    country = normalize_country_name(country)
     if edition_slug in db and country in db[edition_slug]:
         instance_name = get_instance_name(name, year, country)
         instance_daily_data = db[edition_slug][country]['data']
@@ -137,6 +140,7 @@ def instance(name, year, country):
 
 @app.route('/country/<name>')
 def country(name):
+    name = normalize_country_name(name)
     if name in country_data:
         return render_template('country.html', title=u'Wiki Loves Competitions in ' + name, menu=menu,
                                data=country_data[name], country=name)

functions.py
@@ -62,6 +62,10 @@ def get_country_summary(country_data):
             for c in country_data}
 
 
+def normalize_country_name(country_name):
+    return country_name.replace('_', ' ')
+
+
 def get_event_name(event_slug):
     """
     Generate a name from the label.

tests/test_functions.py
@@ -45,6 +45,34 @@ class TestGetEventName(unittest.TestCase):
         self.assertEqual(result, expected)
 
 
+class TestNormalizeCountryName(unittest.TestCase):
+
+    def test_normalize_country_name_one_word(self):
+        result = functions.normalize_country_name('Albania')
+        expected = 'Albania'
+        self.assertEqual(result, expected)
+
+    def test_normalize_country_name_two_words_with_underscores(self):
+        result = functions.normalize_country_name('United_States')
+        expected = 'United States'
+        self.assertEqual(result, expected)
+
+    def test_normalize_country_name_two_words_with_spaces(self):
+        result = functions.normalize_country_name('United States')
+        expected = 'United States'
+        self.assertEqual(result, expected)
+
+    def test_normalize_country_name_three_words_with_underscores(self):
+        result = functions.normalize_country_name('United_Arab_Emirates')
+        expected = 'United Arab Emirates'
+        self.assertEqual(result, expected)
+
+    def test_normalize_country_name_three_words_with_spaces(self):
+        result = functions.normalize_country_name('United Arab Emirates')
+        expected = 'United Arab Emirates'
+        self.assertEqual(result, expected)
+
+
 class TestGetCountrySummary(unittest.TestCase):
 
     def test_get_country_summary(self):

commit 46c54c41507f161bda2e8c39661a499f36912ab8

Fix variables in `users` route
This fixes a bug introduced in d2e8993.

app.py
@@ -107,7 +107,7 @@ def users(name, year, country):
     if not db:
         return index()
     year = '20' + year
-    name = normalize_country_name(name)
+    country = normalize_country_name(country)
     edition_slug = name + year
     if edition_slug in db and country in db[edition_slug]:
         instance_name = get_instance_name(name, year, country)